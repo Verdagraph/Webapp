59f2fd7d909de2899cbfb5eae654c93659f73ce6
root <nathanielarking@gmail.com>
2025-03-06 19:53:22 +0000

    Converter server env validation to arktype

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,111 +1,67 @@
 import { z } from 'zod';
+import {ArkErrors, type} from 'arktype'
+import { Exception } from 'handlebars';
 
-const EnvSchema = z.object({
+/** Default value for JWT key expiry. */
+const defaultKeyExpiry = 3 * 24 * 60 * 60
+
+const EnvSchema = type({
 	/** Email verification. */
-	EMAIL_VERIFICATION_REQUIRED: z
-		.boolean({
-			description:
-				'If true, users will not be able to log in before confirming their email. If false, the email begins verified.'
-		})
-		.default(true),
+	/** If true, users will not be able to log in before confirming their email. If false, the email begins verified. */
+	EMAIL_VERIFICATION_REQUIRED: 'boolean = true',
 
 	/** Triplit. */
-	TRIPLIT_URL: z
-		.string({ description: 'The URL of the Triplit database server.' })
-		.url()
-		.default('http://localhost:6543'),
-	TRIPLIT_SERVER_TOKEN: z
-		.string({
-			description:
-				'The service JWT token provided by triplit for access to the database by the server.'
-		})
-		.default(
-			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ4LXRyaXBsaXQtdG9rZW4tdHlwZSI6InNlY3JldCIsIngtdHJpcGxpdC1wcm9qZWN0LWlkIjoibG9jYWwtcHJvamVjdC1pZCJ9.8Z76XXPc9esdlZb2b7NDC7IVajNXKc4eVcPsO7Ve0ug'
-		),
+	/** The URL of the Triplit database server. */
+	TRIPLIT_URL: 'string.url = "http://localhost:6543"',
+	/** The service JWT token provided by triplit for access to the database by the server. */
+	TRIPLIT_SERVER_TOKEN: 'string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ4LXRyaXBsaXQtdG9rZW4tdHlwZSI6InNlY3JldCIsIngtdHJpcGxpdC1wcm9qZWN0LWlkIjoibG9jYWwtcHJvamVjdC1pZCJ9.8Z76XXPc9esdlZb2b7NDC7IVajNXKc4eVcPsO7Ve0ug"',
+
 
 	/** Networking. */
-	CLIENT_BASE_URL: z
-		.string({ description: 'The base URL which points to the frontend.' })
-		.default('http://localhost:5173'),
-	USING_HTTPS: z
-		.boolean({
-			description: 'True if HTTPS connections are being used with the client.'
-		})
-		.default(false),
-	CLIENT_SAMESITE: z
-		.boolean({
-			description: 'True if the client is served from the same domain as the server.'
-		})
-		.default(false),
-	APP_HOST: z
-		.string({ description: 'The host the server process is running on.' })
-		.default('localhost'),
-	APP_PORT: z
-		.number({ description: 'The port the server process is running on' })
-		.default(8000),
+	/** The base URL which points to the frontend. */
+	CLIENT_BASE_URL: 'string = "http://localhost:5173"',
+	/** True if HTTPS connections are being used with the client. */
+	USING_HTTPS: 'boolean = false',
+	/** True if the client is served from the same domain as the server. */
+	CLIENT_SAMESITE: 'boolean = false',
+	/** The host the server process is running on. */
+	APP_HOST: 'string = "localhost"',
+	/** The port the server process is running on. */
+	APP_PORT: 'number.integer = 8000',
 
 	/** JWTs. */
-	ACCESS_TOKEN_SECRET: z
-		.string({
-			description: 'The secret used to encode JWT access tokens.'
-		})
-		.default('secret'),
-	REFRESH_TOKEN_SECRET: z
-		.string({
-			description: 'The secret used to encode JWT refresh tokens.'
-		})
-		.default('secret'),
-	EMAIL_CONFIRMATION_TOKEN_SECRET: z
-		.string({
-			description: 'The secret used to encode JWT email confirmation tokens.'
-		})
-		.default('secret'),
-	PASSWORD_RESET_TOKEN_SECRET: z
-		.string({
-			description: 'The secret used to encode JWT password reset tokens.'
-		})
-		.default('secret'),
-	REFRESH_TOKEN_EXPIRY_S: z
-		.number({
-			description: 'The expiry duration for JWT refresh tokens, in seconds.'
-		})
-		.default(3 * 24 * 60 * 60),
-	EMAIL_CONFIRMATION_TOKEN_EXPIRY_S: z
-		.number({
-			description: 'The expiry duration for JWT email confirmation tokens, in seconds.'
-		})
-		.default(3 * 24 * 60 * 60),
-	PASSWORD_RESET_TOKEN_EXPIRY_S: z
-		.number({
-			description: 'The expiry duration for JWT password reset tokens, in seconds.'
-		})
-		.default(3 * 24 * 60 * 60),
-	COOKIE_SECRET: z
-		.string({
-			description: 'The secret used to encode cookie signatures.'
-		})
-		.default('secret'),
+	/** The secret used to encode JWT access tokens. */
+	ACCESS_TOKEN_SECRET: 'string = "secret"',
+	/** The secret used to encode JWT refresh tokens. */
+	REFRESH_TOKEN_SECRET: 'string = "secret"',
+	/** The secret used to encode JWT email confirmation tokens. */
+	EMAIL_CONFIRMATION_TOKEN_SECRET: 'string = "secret"',
+	/** The secret used to encode JWT password reset tokens.' */
+	PASSWORD_RESET_TOKEN_SECRET: 'string = "secret"',
+	/** The expiry duration for JWT refresh tokens, in seconds. */
+	REFRESH_TOKEN_EXPIRY_S: type('number.integer').default(defaultKeyExpiry),
+	/** The expiry duration for JWT email confirmation tokens, in seconds. */
+	EMAIL_CONFIRMATION_TOKEN_EXPIRY_S: type('number.integer').default(defaultKeyExpiry),
+	/** The expiry duration for JWT password reset tokens, in seconds. */
+	PASSWORD_RESET_TOKEN_EXPIRY_S: type('number.integer').default(defaultKeyExpiry),
+	/** The secret used to encode cookie signatures. */
+	COOKIE_SECRET: 'string = "secret"',
 
 	/** Emails. */
-	SMTP_HOST: z
-		.string({ description: 'The host of the SMTP server used for sending emails.' })
-		.default('localhost'),
-	SMTP_PORT: z
-		.number({ description: 'The port of the SMTP server used for sending emails.' })
-		.default(1025),
-	SMTP_USERNAME: z
-		.string({ description: "The username of the server's user on the SMTP server" })
-		.default('severSmtpUser'),
-	SMTP_PASSWORD: z
-		.string({ description: "The password of the server's user on the SMTP server." })
-		.default('serverSmtpPassword'),
-	SMTP_SENDER: z
-		.string({
-			description: "The email address of the server's user on the SMTP server."
-		})
-		.email()
-		.default('verdantech@email.com')
-});
+	/** The host of the SMTP server used for sending emails. */
+	SMTP_HOST: 'string = "localhost"',
+	/** The port of the SMTP server used for sending emails. */
+	SMTP_PORT: 'number.integer = 1025',
+	/** The username of the server's user on the SMTP server. */
+	SMTP_USERNAME: 'string = "serverSmtpUser"',
+	/** The password of the server's user on the SMTP server. */
+	SMTP_PASSWORD: 'string = "serverSmtpPassword"',
+	/** The email address of the server's user on the SMTP server. */
+	SMTP_SENDER: 'string.email = "verdantech@email.com"'
+})
 
-const env = EnvSchema.parse(process.env);
+const env = EnvSchema(process.env);
+if (env instanceof ArkErrors) {
+	throw new Exception(env.summary)
+}
 export default env;
