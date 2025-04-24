8a755dd0da32b202703c6039779060721a710c27
nathanielarking <nathanielarking@gmail.com>
2025-04-24 05:36:13 +0000

    Updating calendar wireframes

---
+++ frontend/src/routes/(app)/app/gardens/[gardenId]/(planner)/verdagraph/verdagraph/calendar/CalendarPane.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+    type CalendarItemSection = {
+        label: string
+        icon: string
+        
+    }
+
+    type CalendarItem = {
+        id: string
+        label: string
+        color: string
+        sections: CalendarItemSection[]
+    }
+</script>
